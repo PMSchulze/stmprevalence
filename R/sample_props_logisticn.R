a87dbb480a79fbaa06d6e557b1d747d88d57c162
Patrick Schulze <Pa.Schulze@campus.lmu.de>
2020-06-19 16:14:30 +0200

    add roxygen for logisticn

--- R/sample_props_logisticn.R
+++ R/sample_props_logisticn.R
@@ -1,6 +1,18 @@
-# sample mean and credible interval from LogisticNormal for a given model (stmobj)
-## and full observed range of variable (est_var),
-## holding all variables but est_var as median/majority
+#' Sample from LogisticNormal over full range of variable
+#'
+#' Sample mean and credible interval from LogisticNormal for a given model (stmobj)
+#' and full observed range of variable (est_var),
+#' holding all variables but est_var as median/majority.
+#'
+#' @param stmobj Fitted stm model.
+#' @param est_var Variable for which to sample over full observed range.
+#' @param formula Formula object (the prevalence specification used to fit the stm).
+#' @param metadata Metadata that was used to fit the stm.
+#' @param nsims Number of draws from the LogisticNormal used to obtain empirical mean and quantiles.
+#' @param ci_lower Lower bound of credible interval.
+#' @param ci_upper Upper bound of credible interval.
+#' @param seed Seed.
+#' @export
 sample_props_logisticn <- function(stmobj, est_var, formula, metadata, nsims = 1000,
                                    ci_lower = 0.025, ci_upper = 0.975, seed = NULL) {
   gamma <- stmobj$mu$gamma
